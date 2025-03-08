4a8128ba67737984b951cc9dc755f7e5d2b01ea3
CodePandaaAI <romitsharmakv@gmail.com>
2025-03-08 21:24:44 +0530

    Stable Welcome and SignUp with Navigation

--- app/src/main/java/com/example/findlabour/SignUpScreen.kt
+++ app/src/main/java/com/example/findlabour/SignUpScreen.kt
@@ -22,6 +22,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -116,6 +120,7 @@ fun SignUpScreen() {
 
 @Composable
 private fun InputButton(title: String) {
+    var inputText by remember { mutableStateOf("") }
     Text(
         text = title,
         fontSize = 20.sp,
@@ -125,8 +130,8 @@ private fun InputButton(title: String) {
     )
     Spacer(Modifier.height(16.dp))
     OutlinedTextField(
-        value = "",
-        onValueChange = {},
+        value = inputText,
+        onValueChange = {inputText = it},
         shape = CircleShape,
         modifier = Modifier
             .fillMaxWidth()
